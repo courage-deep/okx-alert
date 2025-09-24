ee01955932d0bbbc2fba1b9d969393c8ca379f72
courage-deep <courage.xf@gmail.com>
2025-09-24 02:04:47 +0000

    从环境读取配置

--- okx_alert.py
+++ okx_alert.py
@@ -5,19 +5,23 @@ import time
 import requests
 
 # OKX ETH 永续合约行情 API
-OKX_API = "https://www.okx.com/api/v5/market/candles?instId=ETH-USDT-SWAP&bar=5m"
+OKX_API = os.getenv("OKX_API","https://www.okx.com/api/v5/market/candles?instId=ETH-USDT-SWAP&bar=5m")
 
 # Bark 推送 API (替换成你自己的 Key)
 BARK_KEY = os.getenv("BARK_KEY")
 
-RUN_DURATION = 60 * 61
+RUN_DURATION = int(os.getenv("RUN_DURATION", 60 * 60 * 4))
 
-CHECK_INTERVAL = 2 
+CHECK_INTERVAL = float(os.getenv("CHECK_INTERVAL", 0.5)) 
 
-CHECK_WAIT = 60 * 15
+CHECK_WAIT = int(os.getenv("CHECK_WAIT", 60 * 15))
 
 # 阈值
-THRESHOLD = 1e8
+THRESHOLD = int(os.getenv("THRESHOLD", 1e8))
+
+AVG_RANGE = int(os.getenv("AVG_MULTI", 5))
+
+AVG_MULTI = int(os.getenv("AVG_MULTI", 3))
 
 
 def format_volume(vol):
@@ -35,7 +39,7 @@ def send_bark(msg):
     try:
         requests.get(url, timeout=10)
     except:
-        print('send back error')
+        print('send bark error')
 
 def volume_alarm():
     msg = None
@@ -46,10 +50,10 @@ def volume_alarm():
     if current_vol > THRESHOLD:
         msg = f"当前: {format_volume(current_vol)}"
     else:
-        vols = [float(item[7]) for item in data[1:6]]
+        vols = [float(item[7]) for item in data[1:AVG_RANGE+1]]
         avg_vol = statistics.mean(vols)
         #print("倒数6到倒数2的平均值:", format_volume(avg_vol))
-        if current_vol >= avg_vol * 3:
+        if current_vol >= avg_vol * AVG_MULTI:
             msg = f" 当前: {format_volume(current_vol)} 均值: {format_volume(avg_vol)}"
     if msg:
         send_bark(msg)
